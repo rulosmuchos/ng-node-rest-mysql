2f37f3bba154f3f8d6525a315685c6ebf9f2df01
rulosmuchos <rulosmuchos@gmail.com>
2020-06-30 15:59:35 -0300

    client build --prod

--- client/src/app/components/navigation/navigation.component.ts
+++ client/src/app/components/navigation/navigation.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from "../../services/auth.service";
 })
 export class NavigationComponent implements OnInit {
 
-  constructor(private authService:AuthService){}
+  constructor(public authService:AuthService){}
 
   ngOnInit() {
   }
--- client/src/app/components/profile/profile.component.ts
+++ client/src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpEventType, HttpEvent } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  edit: boolean = false;
+  selectedFile:File
 
-  constructor() { }
+  progress: number = 0;
+  constructor(
+    private http:HttpClient
+  ) { 
+  }
+  profile: Object = {
+    username: '',
+    password: '',
+    image: '',
+    whapp:'',
+    created_at: new Date()
+  };
 
   ngOnInit(): void {
   }
-
+  onFileChanged(comp_event) {
+    this.selectedFile = comp_event.target.files[0];
+    let input = new FormData();
+    // Add your values in here
+    input.append('file', this.selectedFile);
+    this.http.post('https://app.avellanedacompras.com/upload', input, {
+      reportProgress: true,
+      observe: 'events'}).subscribe((event: HttpEvent<any>) => {
+        switch (event.type) {
+          case HttpEventType.Sent:
+            break;
+          case HttpEventType.ResponseHeader:
+            break;
+          case HttpEventType.UploadProgress:
+            this.progress = Math.round(event.loaded / event.total * 100);
+            break;
+          case HttpEventType.Response:
+            setTimeout(() => {
+              console.log(comp_event);
+              this.profile[comp_event.target.name] = event.body['filename'];
+              this.progress = 0;
+            }, 1500);
+  
+        }
+      });
+    }
 }
