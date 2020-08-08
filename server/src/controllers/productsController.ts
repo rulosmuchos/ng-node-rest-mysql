68be760e03339749a2f47254ea92e2f5efdb3a6d
Andres Grynberg <andi@MacBook-Pro-de-Andres.local>
2020-08-08 18:11:27 -0300

    server enable/trash fn split

--- server/src/controllers/productsController.ts
+++ server/src/controllers/productsController.ts
@@ -40,9 +40,15 @@ class ProductsController {
 
     public async delete(req: Request, res: Response): Promise<void> {
         const { id } = req.params;
+        await pool.query('DELETE FROM products WHERE id = ?', [id]);
+        // await pool.query('UPDATE products SET enable = IF(enable=1, 0, 1) WHERE ID = ?;', [id]);
+        res.json({ message: "The product was deleted" });
+    }
+    public async enable(req: Request, res: Response): Promise<void> {
+        const { id } = req.params;
         // await pool.query('DELETE FROM products WHERE id = ?', [id]);
         await pool.query('UPDATE products SET enable = IF(enable=1, 0, 1) WHERE ID = ?;', [id]);
-        res.json({ message: "The product was deleted" });
+        res.json({ message: "The product was toggled enabled" });
     }
     public async trash(req: Request, res: Response): Promise<void> {
         const { id } = req.params;
